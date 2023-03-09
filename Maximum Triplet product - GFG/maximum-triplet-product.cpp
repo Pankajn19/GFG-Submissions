6cb8592d2ecf43d844e8b58cd579947128fa30ff
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-03-10 00:13:56 +0530

    Added solution - LeetHub

--- Maximum Triplet product - GFG/maximum-triplet-product.cpp
+++ Maximum Triplet product - GFG/maximum-triplet-product.cpp
@@ -12,28 +12,9 @@ class Solution {
   public:
     long long maxTripletProduct(long long arr[], int n)
     {
-        if(n==3) return (arr[0]*arr[1]*arr[2]);
+     //   if(n==3) return (arr[0]*arr[1]*arr[2]);
         sort(arr,arr+n);
         return max(arr[0]*arr[1]*arr[n-1], arr[n-1]*arr[n-2]*arr[n-3]);
-        
-       /* int neg = 0;
-        for(int i=0;i<n;i++){
-            if(arr[i]<0) neg++;
-        }
-        if(neg<=1){
-            priority_queue<long long> pq(arr,arr+n);
-            long long x = pq.top();
-            pq.pop();
-            long long y = pq.top();
-            pq.pop();
-            long long z = pq.top();
-            pq.pop();
-            return (x*y*z);
-        }
-         long long y = INT_MIN;
-         for(int i=0;i<n;i++) y = max(y,arr[i]);
-         
-         */
          
     }
 };
