106728932aebf9928f92fccd3f20ae0294b5c6e9
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-05-20 03:22:15 +0530

    Time: 29 ms (51.34%), Space: 7.3 MB (82.77%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int change(int amt, vector<int>& c) {
       int n = c.size();
-      vector<vector<int>> dp(n,vector<int>(amt+1,0));
+      vector<int> prev(amt+1,0), cur(amt+1,0);
       for(int i=0;i<=amt;i++){
-        if(i%c[0]==0) dp[0][i] = 1;
+        if(i%c[0]==0) prev[i] = 1;
       }
       for(int i=1;i<n;i++){
         for(int j=0;j<=amt;j++){
-          int ntk = dp[i-1][j];
+          int ntk = prev[j];
           int tk = 0;
-          if(c[i]<=j) tk =  dp[i][j-c[i]];      
-          dp[i][j] = tk + ntk;
+          if(c[i]<=j) tk =  cur[j-c[i]];      
+          cur[j] = tk + ntk;
 
         }
+          prev = cur;
       }
-      return dp[n-1][amt];
+      return prev[amt];
 
      // return rec(n-1,c,amt,dp);
         
