25306a6455a101361a733d8ea274ba6b041a9357
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-05-20 03:49:17 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,18 +9,22 @@ using namespace std;
 
 class Solution{
 public:
-    int rec(int ind, int val[], int wt[], int w, int n,vector<vector<int>>& dp){
-        if(ind==0) return val[0] * (w/wt[0]);
-        if(dp[ind][w]!=-1) return dp[ind][w];
-        int tk = 0;
-        if(wt[ind]<=w) tk =  val[ind] + rec(ind,val,wt,w-wt[ind],n,dp);
-        int ntk = rec(ind-1,val,wt,w,n,dp);
-        return dp[ind][w] = max(tk,ntk);
-    }
     int knapSack(int n, int w, int val[], int wt[])
     {
-        vector<vector<int>> dp(n,vector<int>(w+1,-1));
-        return rec(n-1,val,wt,w,n,dp);
+        vector<vector<int>> dp(n,vector<int>(w+1,0));
+        for(int i=0;i<=w;i++) dp[0][i] = val[0] * (i/wt[0]); 
+        for(int i=1;i<n;i++){
+            for(int j=0;j<w+1;j++){
+                 int tk = 0;
+                 if(wt[i]<=j) tk =  val[i] + dp[i][j-wt[i]];
+                 int ntk = dp[i-1][j];
+                 dp[i][j] = max(tk,ntk);
+                
+            }
+        }
+        return dp[n-1][w];
+        
+        //return rec(n-1,val,wt,w,n,dp);
        
     }
 };
