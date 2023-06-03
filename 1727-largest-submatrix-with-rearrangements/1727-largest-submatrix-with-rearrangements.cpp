3df3e01b8785cb78937247e7aa3e0d2733118c18
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-06-03 17:51:04 +0530

    Time: 224 ms (72.84%), Space: 65.8 MB (95.06%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-   
-    // return psm[6];
     
     int largestSubmatrix(vector<vector<int>>& matrix) {
       int n = matrix.size();
       int m = matrix[0].size();
-      vector<vector<int>> ans(n,vector<int>(m,0));
      for(int i=0;i<m;i++){
         int k = 0;
         while(k<n){
         while(k<n && !matrix[k][i]) k++;
-        if(k<n) ans[k][i] = 1;
+        if(k<n) matrix[k][i] = 1;
         int l = k+1;
         while(l<n && matrix[l][i]){
-          ans[l][i] = ans[l-1][i] + 1;
+          matrix[l][i] = matrix[l-1][i] + 1;
           l++;
         }
         k=l+1;
@@ -23,17 +20,9 @@ public:
       
       int answer = 0;
       for(int i=0;i<n;i++){
-        sort(ans[i].begin(), ans[i].end(), greater<int>());
-       // reverse(cur.begin(), cur.end());
-        for(int j=0;j<m;j++) answer= max( answer, (ans[i][j]*(j+1)));
+        sort(matrix[i].begin(), matrix[i].end(), greater<int>());
+        for(int j=0;j<m;j++) answer= max( answer, (matrix[i][j]*(j+1)));
       }
-  /*    if(n==3 && m==3 ){
-        return ans[2][2];
-
-      }
-      */
-      
-     // return ans[0][0];
       return answer;      
     }
 };
