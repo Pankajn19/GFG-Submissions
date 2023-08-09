9ec8a07c1c11e4c195280fdc0bf1152a154d191c
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-08-10 05:28:39 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -9,25 +9,16 @@ using namespace std;
 class Solution
 {
     public:
-   /* int rec(int i1, int i2, string s1, string s2, int x, int y, vector<vector<int>>& dp){
-        if(i1==-1 || i2==-1) return 0;
-        if(dp[i1][i2]!=-1) return dp[i1][i2];
-        if(s1[i1]==s2[i2])  return dp[i1][i2] = 1 + rec(i1-1,i2-1,s1,s2,x,y,dp);
-        return dp[i1][i2] = max(rec(i1-1,i2,s1,s2,x,y,dp), rec(i1,i2-1,s1,s2,x,y,dp));
-        
-    }*/
-    int lcs(int x, int y, string s1, string s2)
+    int lcs(int n, int m, string s1, string s2)
     {
-        vector<int> prev(y+1,0), cur(y+1,0);
-        for(int i1=1;i1<=x;i1++){
-            for(int i2=1;i2<=y;i2++){
-                if(s1[i1-1]==s2[i2-1])   cur[i2] = 1 + prev[i2-1];
-                else cur[i2] = max(prev[i2], cur[i2-1]);
-                
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(s1[i]==s2[j]) dp[i+1][j+1] = 1 + dp[i][j];
+                else dp[i+1][j+1] = max(dp[i][j+1],dp[i+1][j]);
             }
-            prev = cur;
         }
-        return prev[y];
+        return dp[n][m];
     }
 };
 
@@ -35,15 +26,15 @@ class Solution
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
